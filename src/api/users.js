e38e477098b869e51080d8509466faecdb531a0d
Soham Morye <moryesoham4@gmail.com>
2026-07-25 17:27:29 +0530

    Delete src directory

--- src/api/users.js
+++
@@ -1,7 +0,0 @@
-import client from './client'
-
-export const listUsers = () => client.get('/api/users/').then((r) => r.data)
-export const listUsersByEvent = (eventId) => client.get(`/api/users/event/${eventId}`).then((r) => r.data)
-export const assignRole = (data) => client.post('/api/users/assign-role', data).then((r) => r.data)
-export const resetPassword = (data) => client.post('/api/users/reset-password', data).then((r) => r.data)
-export const getAuditLog = () => client.get('/api/users/audit-log').then((r) => r.data)
