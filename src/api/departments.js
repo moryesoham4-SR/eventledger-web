789fef3c860d87eacb9fbcb47586bd992a66ab82
Soham Morye <moryesoham4@gmail.com>
2026-07-26 08:03:06 +0530

    Delete src directory

--- src/api/departments.js
+++
@@ -1,5 +0,0 @@
-import client from './client'
-
-export const listDepartments = (eventId) => client.get('/api/departments/', { params: { event_id: eventId } }).then((r) => r.data)
-export const createDepartment = (data) => client.post('/api/departments/', data).then((r) => r.data)
-export const deleteDepartment = (id) => client.delete(`/api/departments/${id}`).then((r) => r.data)
