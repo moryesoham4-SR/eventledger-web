29feabdc1bce14321c5cf265356173eecad9e5a5
Soham Morye <moryesoham4@gmail.com>
2026-08-02 11:59:59 +0530

    Delete src directory

--- src/api/auth.js
+++
@@ -1,7 +0,0 @@
-import client from './client'
-
-export const login = (email, password) =>
-  client.post('/api/auth/login', { email, password }).then((res) => res.data)
-
-export const register = (name, email, password, org_name = '') =>
-  client.post('/api/auth/register', { name, email, password, org_name }).then((res) => res.data)
