b5802775b314c6c5e21673a44025e97c3bb0b329
Soham Morye <moryesoham4@gmail.com>
2026-08-11 20:41:12 +0530

    Delete src directory

--- src/api/sponsors.js
+++
@@ -1,5 +0,0 @@
-import client from './client'
-
-export const listSponsors = (eventId) => client.get('/api/sponsors/', { params: { event_id: eventId } }).then((r) => r.data)
-export const createSponsor = (data) => client.post('/api/sponsors/', data).then((r) => r.data)
-export const deleteSponsor = (id) => client.delete(`/api/sponsors/${id}`).then((r) => r.data)
